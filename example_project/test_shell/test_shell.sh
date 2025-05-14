5cad8d46fa69f3befea05a9a3cc37108a37bb0e6
Veysel KANTARCILAR <41362595+kantrveysel@users.noreply.github.com>
2025-05-14 07:00:25 +0300

    Update test_shell.sh

--- example_project/test_shell/test_shell.sh
+++ example_project/test_shell/test_shell.sh
@@ -6,7 +6,6 @@ echo -e "\e[1;34m🔧 Starting...\e[0m"
 sleep 1
 
 for (( i=0; i <= $max; ++i )); do
-    # Renkli ve animasyonlu çıktı
     echo -ne "\e[1;32m[$i/$max] ➤ \e[0m"
     echo -e "\e[1;36mOutput $i In Progress...\e[0m"
     sleep 0.5
