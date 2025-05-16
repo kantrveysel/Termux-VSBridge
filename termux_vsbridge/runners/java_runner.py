e3fbe3bab7d9b4f4e564b23caed722d46f00ebc9
Veysel KANTARCILAR <41362595+kantrveysel@users.noreply.github.com>
2025-05-16 21:17:55 +0300

    Modular Java runner

--- termux_vsbridge/runners/java_runner.py
+++ termux_vsbridge/runners/java_runner.py
@@ -1,27 +1,43 @@
 from . import SSHClient, SFTPClient, os
 
+
 class JavaRunner:
-    """Java Runner"""
+    """Java Project Runner"""
+
     def __init__(self, config_manager):
         self.config_manager = config_manager
-        self.ssh_client = SSHClient(
-            config_manager.get("host"),
-            config_manager.get("port"),
-            config_manager.get("username"),
-            config_manager.get("password")
-        )
-        self.sftp_client = SFTPClient(
-            config_manager.get("host"),
-            config_manager.get("port"),
-            config_manager.get("username"),
-            config_manager.get("password")
-        )
+        self._setup_clients()
 
-    def run(self, local_file, remote_path):
-        """Java Compiler &  Runner"""
-        self.sftp_client.push(local_file, remote_path)
+    def _setup_clients(self):
+        """Initialize SSH and SFTP clients"""
+        credentials = (
+            self.config_manager.get("host"),
+            self.config_manager.get("port"),
+            self.config_manager.get("username"),
+            self.config_manager.get("password")
+        )
+        self.ssh_client = SSHClient(*credentials)
+        self.sftp_client = SFTPClient(*credentials)
+
+    def _extract_paths(self, remote_path):
+        """Extract remote directory and class name"""
         remote_dir = os.path.dirname(remote_path).replace('\\', '/')
         class_name = os.path.splitext(os.path.basename(remote_path))[0]
-        self.ssh_client.connect()
-        self.ssh_client.execute(f"cd {remote_dir} && javac {os.path.basename(remote_path)}")
+        return remote_dir, class_name
+
+    def _compile_java(self, remote_dir, filename):
+        """Compile Java file on remote"""
+        self.ssh_client.execute(f"cd {remote_dir} && javac {filename}")
+
+    def _run_java(self, remote_dir, class_name):
+        """Run Java class on remote"""
         self.ssh_client.execute(f"cd {remote_dir} && java {class_name}")
+
+    def run(self, local_file, remote_path):
+        """Main entry: Compile & Run Java source"""
+        self.sftp_client.push(local_file, remote_path)
+        remote_dir, class_name = self._extract_paths(remote_path)
+
+        self.ssh_client.connect()
+        self._compile_java(remote_dir, os.path.basename(remote_path))
+        self._run_java(remote_dir, class_name)
